d213343e7c1360b6f80fa7561e250524f6a8cede
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-12-02 18:46:36 -0600

    clean up the test run code a bit

--- tasks/test-run-client.js
+++ tasks/test-run-client.js
@@ -5,76 +5,15 @@
  * performance results.
  *
  * assumes that the other servers are already running.
- *
- * this code is unforgivably ugly
  */
-function trim(s) {
-  return s.replace(/\s+$/, '').replace(/^\s+/, '');
-}
-
-function notGoogle(result) {
-  return result.engine !== 'google';
-}
-
-function avg(array) {
-  return array.reduce(function(sum, x) {
-    return sum + x;
-  }, 0) / array.length;
-}
 
 module.exports = function() {
-  var fs = require('fs');
-  var qs = require('querystring');
-  var _ = require('lodash');
-  var Q = require('q');
-  var request = Q.denodeify(require('request'));
-  var readFile = Q.denodeify(fs.readFile);
+  var testRun = require('./../lib/test-run');
   var taskDone = this.async();
 
-  readFile('etc/searches.txt')
-    .then(function(searches) {
-      return trim(searches.toString())
-        .split('\n')
-        .map(function(term) {
-          return function(previousResult) {
-            var url = 'http://localhost:2999?q=' + qs.escape(term);
-            console.log('requesting:',url);
-            return request(url)
-              .spread(function(response, body) {
-                // console.log(body);
-                var result = JSON.parse(body);
-                result.term = term;
-                // console.log(previousResult);
-                return previousResult?
-                  previousResult.concat([result]) : [result];
-              });
-          };
-        })
-        .reduce(Q.when, Q());
-    })
-    .then(function(runs) {
-      var totals = runs.reduce(function(totals, run) {
-        run.filter(notGoogle)
-          .forEach(function(result) {
-            var engineTotals =
-              totals[result.engine] = totals[result.engine] || {};
-            (engineTotals.ap = engineTotals.ap || []).push(result.ap);
-            (engineTotals.ndcg = engineTotals.ndcg || []).push(result.ndcg);
-          });
-        return totals;
-      }, {});
-
-      var ranking = {
-        map: _.sortBy(_.map(totals, function(total, engine) {
-            return {engine: engine, map: avg(total.ap)};
-          }), 'map').reverse(),
-        mndcg: _.sortBy(_.map(totals, function(total, engine) {
-            return {engine: engine, mndcg: avg(total.ndcg)};
-          }), 'mndcg').reverse()
-      };
-
-      console.log(JSON.stringify(totals, null, '  '));
-      console.log(JSON.stringify(ranking, null, '  '));
+  testRun()
+    .then(function(result) {
+      console.error(JSON.stringify(result.ranking, null, '  '));
       taskDone();
     })
     .done();
